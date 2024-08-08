d7d73ed9ffd08b4a299268a75d6dc3d60fd4f21b
mmjck <msantosjck@gmail.com>
2024-08-08 11:43:04 -0400

    feat: added token service

---
+++ ms/src/main/java/com/selaz/ms/infra/config/security/TokenService.java
@@ -0,0 +1,58 @@
+package com.selaz.ms.infra.config.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.selaz.ms.core.domain.user.entity.User;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    private static String SERVICE_NAME = "auth-service";
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer(SERVICE_NAME)
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+
+            return token;
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Error while aithentication");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer(SERVICE_NAME)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
