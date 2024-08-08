db9a01283717071a92ba431a06ea1cf2f6d00915
mmjck <msantosjck@gmail.com>
2024-08-08 11:43:52 -0400

    feat: added swagger config

---
+++ ms/src/main/java/com/selaz/ms/infra/config/swagger/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.selaz.ms.infra.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .name("Authorization")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
+                .info(new Info().title("API Documentation")
+                        .version("1.0")
+                        .description("API documentation with Swagger"));
+    }
+}
